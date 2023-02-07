612f6bc9bd140a0ef6fd4a7fe93c12ed65cd59ab
EnayetHossain <enayethossasin135@gmail.com>
2023-02-07 13:31:26 +0600

    responsive features section and animated tab

--- js/app.js
+++ js/app.js
@@ -9,3 +9,25 @@ hamBar.addEventListener("click", ()=>{
 hamClose.addEventListener("click", ()=>{
     hamBarMenu.style.left = "100%";
 });
+
+const tabBtns = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".content");
+const underline = document.querySelector(".underline");
+
+tabBtns.forEach((tab, index)=>{
+    tab.addEventListener("click", (e)=>{
+
+        tabBtns.forEach(tab => { 
+            tab.classList.remove("active"); 
+        });
+        tab.classList.add("active");
+        underline.style.width = `${e.target.offsetWidth}px`;
+        underline.style.left = `${e.target.offsetLeft}px`;
+
+        contents.forEach(content => {
+            content.classList.remove("active");
+        });
+        contents[index].classList.add("active");
+    });
+
+});
